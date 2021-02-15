5c4092bc975b5cfb01b12b15fac643454452f6e2
karljanar <25798556+karljanar@users.noreply.github.com>
2021-02-15 18:32:45 +0200

    Kodutoo 2

---
+++ w02_klass/kodutoo2.java
@@ -0,0 +1,47 @@
+import java.util.Hashtable;
+
+public class kodutoo2 {
+    Hashtable<String, Double> resistors = new Hashtable<String, Double>();
+    public Hashtable<String, Double> askResistors(){return resistors;}
+
+    public void addResistors(String resistor, double resistance){
+        if(resistors.containsKey(resistor)){
+            throw new RuntimeException(resistor + " juba olemas.");
+        } else {
+            resistors.put(resistor, resistance);
+        }
+    }
+
+    public double totalResitance(String type, int voltage){
+        double tR = 0;
+        if(type.equals("serial")) {
+            for (double i : resistors.values()) {
+                tR += i;
+            }
+        } else {
+            for(double i : resistors.values()){
+
+                tR += Math.pow(i, -1);
+            }
+            tR = (double) Math.round(Math.pow(tR, -1) * 100d) / 100d;
+        }
+        return tR;
+    }
+
+    public double current(String type, int voltage){
+        double tResistance = totalResitance(type, voltage);
+        double elecCurrent = voltage / tResistance;
+        elecCurrent = (double)Math.round(elecCurrent * 100000d) / 100000d;
+        return elecCurrent;
+    }
+
+    public double power(String type, int voltage) {
+        double elecPower = voltage * current(type, voltage);
+        elecPower = (double) Math.round(elecPower * 100000d) / 100000d;
+        return elecPower;
+    }
+
+
+
+
+}
---
+++ w02_klass/kodutooproov.java
@@ -0,0 +1,27 @@
+public class kodutooproov {
+    public static void main(String[] arg){
+        kodutoo2 serialConnection = new kodutoo2();
+        serialConnection.addResistors("r1", 330.0);
+        serialConnection.addResistors("r2", 330.0);
+        serialConnection.addResistors("r3", 330.0);
+
+        System.out.println(serialConnection.askResistors());
+        System.out.println(serialConnection.totalResitance("serial", 5) + "Ω");
+        System.out.println("Elektriahela voolutugevuseks on: " + serialConnection.current("serial", 5) + "A");
+        System.out.println("Elektriahela voolutugevuseks on: " + serialConnection.power("serial", 5) + "W");
+
+
+
+        kodutoo2 parallelConnection = new kodutoo2();
+        parallelConnection.addResistors("r1", 20.0);
+        parallelConnection.addResistors("r2", 33.0);
+        parallelConnection.addResistors("r3", 33.0);
+
+        System.out.println(parallelConnection.askResistors());
+        System.out.println(parallelConnection.totalResitance("parallel", 5) + "Ω");
+        System.out.println("Elektriahela voolutugevuseks on: " + parallelConnection.current("parallel", 5) + "A");
+        System.out.println("Elektriahela voolutugevuseks on: " + parallelConnection.power("parallel", 5) + "W");
+
+
+    }
+}
